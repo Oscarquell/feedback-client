19be7cf75b17486213dce487bb584644de34caa1
Oscarquell <oscarquell96@gmail.com>
2023-07-01 02:56:33 +0600

    added url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://feedback-server.vercel.app/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
