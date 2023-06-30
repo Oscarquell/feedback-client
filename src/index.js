46cb7b3556f64848ff03c768374bfd5797c08973
Oscarquell <oscarquell96@gmail.com>
2023-07-01 01:41:03 +0600

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://argen-lilac.vercel.app/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
