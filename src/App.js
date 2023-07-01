292cf196136e46a7c5ee9ff99123c2a3f9ce6277
Oscarquell <oscarquell96@gmail.com>
2023-07-01 15:33:48 +0300

    added loader

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import FirstSection from "./components/firstSection";
 import SecondSection from "./components/secondSection";
 import Footer from "./components/footer";
 import TimerBlock from "./components/timerBlock/timer";
+import CircularIndeterminate from "./components/loader";
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+    }, []);
+
   return (
     <div className="App">
-        <FirstSection />
-        <SecondSection />
-        <TimerBlock />
-        <Footer />
+        {
+            isLoading ?
+                <CircularIndeterminate />
+                :
+                <div>
+                    <FirstSection />
+                    <SecondSection />
+                    <TimerBlock />
+                    <Footer />
+                </div>
+        }
+
     </div>
   );
 }
