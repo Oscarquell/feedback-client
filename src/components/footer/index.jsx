5666a5c4746769718f6a97efae8661c0289fca34
Oscarquell <oscarquell96@gmail.com>
2023-07-05 00:40:01 +0600

    test yandex taxi

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,17 +5,18 @@ const Footer = () => {
     return (
         <div className='footer'>
 
-            <div>
-                проезд до ресторана
-                <a target="_blank" href="https://go.2gis.com/uzipr">2GIS</a>
-            </div>
-            <div>
-                проезд до ресторана
-                <a target="_blank" href="https://goo.gl/maps/cvaGhSYcN7sXZu178">Google Maps</a>
-            </div>
+            {/*<div>*/}
+            {/*    проезд до ресторана*/}
+            {/*    <a target="_blank" href="https://go.2gis.com/uzipr">2GIS</a>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    проезд до ресторана*/}
+            {/*    <a target="_blank" href="https://goo.gl/maps/cvaGhSYcN7sXZu178">Google Maps</a>*/}
+            {/*</div>*/}
 
             <div>
-                <a href="yandextaxi://route?source=app_search&dest=улица+Токтоболота+Абдумомунова,+дом+220А">Заказать такси</a>
+                <a href="yandextaxi://route?source=app_search&dest=%D1%83%D0%BB%D0%B8%D1%86%D0%B0%20%D0%A2%D0%BE%D0%BA%D1%82%D0%BE%D0%B1%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%20%D0%90%D0%B1%D0%B4%D1%83%D0%BC%D0%BE%D0%BC%D1%83%D0%BD%D0%BE%D0%B2%D0%B0%20%D0%B4%D0%BE%D0%BC%20220%D0%90">Заказать такси</a>
+
             </div>
 
             <div className="footer-title">
